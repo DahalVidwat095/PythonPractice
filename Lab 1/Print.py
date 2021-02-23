c1bb4817246e7764e03fffa101e885750c05c8d5
Vidwat <dahalvidwat403@gmail.com>
2021-02-23 12:09:25 +0545

    This program teaches various ways to print.

---
+++ Lab 1/Print.py
@@ -0,0 +1,7 @@
+name=input("enter name:")
+age=int(input("enter age:"))
+print(f"My age is {age} and my name is {name}.")
+print("My name is",name,"and my age is",age,".")
+print("Hello my name is" +name+ "and i am" +str(age) +"years old")
+print("Hello my name is %s and i am % d years old" %(name,age))
+print("Hello my name is {} and i am {} years old".format(name,age))
