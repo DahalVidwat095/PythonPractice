fdf1ba0ffbde1e534b5d0962ef36d209185a67e7
Vidwat <dahalvidwat403@gmail.com>
2021-02-23 11:26:34 +0545

    Initial commit

---
+++ Lab 1/addition.py
@@ -0,0 +1,6 @@
+#Write a program that takes three numbers and prints their sum.Every number is given in a seperate line .
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+num3=int(input("enter third number:"))
+Sum=num1+num2+num3
+print(f"The sum of the given number is {Sum}")
