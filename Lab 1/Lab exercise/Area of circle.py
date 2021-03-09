85b42a300cc61899461f657a12b15ee38b9ebc00
Vidwat <dahalvidwat403@gmail.com>
2021-03-09 12:10:31 +0545

    Program for area of circle

---
+++ Lab 1/Lab exercise/Area of circle.py
@@ -0,0 +1,5 @@
+'''Write a Python program which accepts the radius of a circle from the user and compute the area.
+ (area of circle=PI*r**2)'''
+Radius=int(input("Enter the radius:"))
+Area=(3.14*(Radius)**2)
+print(f"The area of circle is {Area} sq. units.")
