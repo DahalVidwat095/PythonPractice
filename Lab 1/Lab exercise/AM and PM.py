ef504ee348f6df2b3bacc4746691de58ab776634
Vidwat <dahalvidwat403@gmail.com>
2021-03-16 11:16:17 +0545

    Sum of positive integers

---
+++ Lab 1/Lab exercise/AM and PM.py
@@ -0,0 +1,4 @@
+a=2
+b=3
+c=a+b
+print(" The sum of {2} and {1} is {0}".format(c,b,a))
---
+++ Lab 1/Lab exercise/Sum of integers.py
@@ -0,0 +1,8 @@
+'''
+9.Write a python program to find sum of first n positive integers.
+sum= (n*(n+1))/2
+'''
+#############################################################################################
+n= int(input("Enter a positive integer:"))
+sum= (n*(n+1)/2)
+print(f"The sum of first positive integers is {sum}")
