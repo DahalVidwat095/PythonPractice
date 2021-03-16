5047588d0754a328441d852e7701f8c62b594659
Vidwat <dahalvidwat403@gmail.com>
2021-03-16 11:52:32 +0545

    Salary

--- Lab 1/Lab exercise/indexing.py
+++ Lab 1/Lab exercise/indexing.py
@@ -1,6 +1,6 @@
 ''' A person has a basic salary 20000.He spends 10% basic salary as expenses .Wite a program to find
  net salary after his expenses '''
-salary=
+salary=20000
 expenses=(10*20000)/100
 net_salary=salary-expenses
 print(net_salary)
