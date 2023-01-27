fa5a78963f5a75886e334e8122be22ef375035e4
Vidwat Dahal <dahalvidwat403@gmail.com>
2023-01-27 21:08:36 +0545

    Changes were made

--- Lab 1/Lab exercise/addition.py
+++ Lab 1/Lab exercise/addition.py
@@ -3,4 +3,3 @@ num1=int(input("enter first number:"))
 num2=int(input("enter second number:"))
 num3=int(input("enter third number:"))
 Sum=num1+num2+num3
-print(f"The sum of the given number is {Sum}")
